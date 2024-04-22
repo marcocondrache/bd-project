97e1caf1b076308da791c0d1a6fd452a82caebb3
Diego Caspi <diego.caspi@danfoss.com>
2024-04-22 10:54:03 +0200

    chore: added products and products categories models + migrations

--- app/modules/products/models.py
+++ app/modules/products/models.py
@@ -1,7 +1,14 @@
+from __future__ import annotations
+
+from sqlalchemy import Table
 from sqlalchemy.orm import Mapped, mapped_column
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, List
+from sqlalchemy.dialects.postgresql import UUID
 
 from extensions import db
+import uuid
+
+from extensions import Base
 
 if TYPE_CHECKING:
     from app.modules.sellers.models import Seller
@@ -9,10 +16,35 @@ else:
     Seller = "Seller"
 
 
+class ProductCategory(db.Model):
+    __tablename__ = "product_categories"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, nullable=False, index=True)
+    guid: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), default=uuid.uuid4, unique=True, nullable=False)
+    name: Mapped[str] = mapped_column(db.String(255), nullable=False, unique=True)
+    created_at: Mapped[str] = mapped_column(db.DateTime, nullable=False, server_default=db.func.now())
+    updated_at: Mapped[str] = mapped_column(
+        db.DateTime, nullable=False, server_default=db.func.now(), onupdate=db.func.now()
+    )
+    deleted_at: Mapped[str] = mapped_column(db.DateTime, nullable=True)
+
+    def __repr__(self):
+        return f"<ProductCategory name={self.name} description={self.description}>"
+
+
+association_table = Table(
+    "product_categories_association",
+    Base.metadata,
+    db.Column("product_id", db.ForeignKey("products.id")),
+    db.Column("product_category_id", db.ForeignKey("product_categories.id")),
+)
+
+
 class Product(db.Model):
     __tablename__ = "products"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, nullable=False, index=True)
+    guid: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), default=uuid.uuid4, unique=True, nullable=False)
     owner_seller_id: Mapped[int] = mapped_column(db.Integer, db.ForeignKey("sellers.id"), nullable=False)
     name: Mapped[str] = mapped_column(db.String(255), nullable=False)
     description: Mapped[str] = mapped_column(db.String(255), nullable=True)
@@ -30,7 +62,9 @@ class Product(db.Model):
 
     seller: Mapped["Seller"] = db.relationship("Seller", back_populates="products")
 
+    categories: Mapped[List[ProductCategory]] = db.relationship(ProductCategory, secondary=association_table, back_populates="products")
+
     def __repr__(self):
-        return (f"<Product owner_seller_id={self.owner_seller_id} name={self.name} description={self.description} "
+        return (f"<Product guid={self.guid} owner_seller_id={self.owner_seller_id} name={self.name} description={self.description} "
                 f"brand={self.brand} is_second_hand={self.is_second_hand} sequence={self.sequence} price={self.price} "
                 f"currency={self.currency} stock={self.stock}>")
---
+++ app/modules/products/views.py
@@ -0,0 +1,27 @@
+from flask_login import login_required, current_user
+from flask import request
+
+from app.modules.products import products
+from app.modules.products.handlers import create_product
+
+
+@products.route('', methods=['GET, POST'])
+@login_required
+def user_products():
+    if request.method == 'POST':
+        seller_id = current_user.id
+        name = request.json.get('name')
+        price = request.json.get('price')
+        stock = request.json.get('stock')
+        categories = request.json.get('categories')
+        description = request.json.get('description')
+        brand = request.json.get('brand')
+        is_second_hand = request.json.get('is_second_hand')
+
+        product = create_product(seller_id, name, price, stock, categories, description, brand, is_second_hand)
+        if not product:
+            return {'message': 'seller not found'}, 404
+
+        return {'message': 'product created'}, 200
+    else:
+        return {'message': 'user products'}, 200
