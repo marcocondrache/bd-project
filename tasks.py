5a58c6ecb8845323a385bcac9fd64a7266a9d29e
marcocondrache <marcomihaicondrache@gmail.com>
2024-05-02 20:48:34 +0200

    feat: add pre loading screen

--- tasks.py
+++ tasks.py
@@ -1,9 +1,17 @@
+import os
+
 from invoke import task
 
 
 @task
 def setup(c):
+    # Install deps
     c.run("pip install -r requirements.txt")
+
+    # Generate secret and other env variables
+    env = open('.env', 'w+')
+    env.write("SECRET_KEY={secret}".format(secret=os.urandom(32)))
+
 
 
 @task
