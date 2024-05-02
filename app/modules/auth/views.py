308c94190e66d500cb537ed1c15cdab07916e2e4
marcocondrache <marcomihaicondrache@gmail.com>
2024-05-02 19:13:48 +0200

    feat: add corse and fix csrf

--- app/modules/auth/views.py
+++ app/modules/auth/views.py
@@ -4,6 +4,7 @@ from flask_login import login_user, login_required, logout_user
 from app.modules.auth import auth
 from app.modules.auth.forms import LoginForm
 from app.modules.auth.handlers import validate_user, get_user_by_email, register_user
+from extensions import csrf
 
 
 @auth.route("/login", methods=["GET", "POST"])
@@ -27,6 +28,7 @@ def login():
 
 
 @auth.route("/signup", methods=["GET", "POST"])
+@csrf.exempt
 def signup():
     if request.method == "POST":
         email = request.form.get('email')
